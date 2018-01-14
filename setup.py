62b4a590b0cdfbf6cf369575d4205388c418d6ab
Felix Lechner <felix.lechner@lease-up.com>
2018-01-14 14:49:38 -0800

    Manpages from Debian (#66)
    * Manpages from Debian packaging.
    
    * Install manpages via setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,14 @@ setup(
   packages=['libpius'],
   requires=['six'],
   scripts=['pius', 'pius-keyring-mgr', 'pius-party-worksheet', 'pius-report'],
+  data_files=[
+    (
+      'share/man/man1', [
+        'doc/pius-keyring-mgr.1',
+        'doc/pius.1',
+        'doc/pius-report.1',
+        'doc/pius-party-worksheet.1'
+      ]
+    )
+  ]
 )
